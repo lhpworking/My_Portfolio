0cb8f142c8280e6685a3d118851dfe1d182ed894
Phats Lys <lhpworking@gamil.com>
2021-11-08 21:30:52 +0700

    Ui version 1.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,53 @@
+gsap.registerPlugin(ScrollTrigger);
+// controller about myself
+const projectContainer = document.querySelector(".main__project");
+const myselfContainer = document.querySelector(".main__myself");
+const myself = document.querySelectorAll(".main__myself h1");
+const myselfBtn = document.querySelector(".main__myself button");
+const mainImg = document.querySelector(".main__myself-img img");
+const projectImg = document.querySelector(".main__project-img img");
 
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector(".intro-text");
+/** animation: section myself */
+gsap.fromTo(
+  myself,
+  { x: -400, y: 0, duration: 4, opacity: 0, ease: Bounce },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+);
+gsap.fromTo(
+  myselfBtn,
+  { x: 0, y: 200, duration: 4, opacity: 0, ease: Bounce },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+);
+gsap.fromTo(
+  mainImg,
+  { x: 0, y: -400, duration: 4, opacity: 0, ease: Bounce },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+);
 
-// init controller
-const introController = new ScrollMagic.Controller();
-// create a scene
-//scene video
-var sceneVideo = new ScrollMagic.Scene({
-    duration: 7300, // the scene should last for a scroll distance of 100px
-    triggerElement: intro,
-    triggerHook: 0,
-})
-    .setPin(intro) // pins the element for the the scene's duration
-    .addTo(introController); // assign the scene to the controller
-
- //scene text intro 
-let textAnimated = new TweenMax.fromTo(text, 6, { opacity: 0 }, { opacity: 1 });
-var sceneText = new ScrollMagic.Scene({
-    duration: 6000, // the scene should last for a scroll distance of
-    triggerElement: intro,
-    triggerHook: 0,
-})
-    .setTween(textAnimated)
-    .addTo(introController);
-
- // animate the scene
-let accelAmount = 0.1;
-let scrollPosition = 0;
-let delay = 0;
-
-sceneVideo.on("update", e => {
-    scrollPosition = e.scrollPos / 1000;
+// overlay
+TweenMax.to(".overlay", 4, {
+  top: "-100%",
+  ease: Expo.easeInOut,
 });
 
-setInterval(() => {
-    delay += (scrollPosition - delay) * accelAmount;
-    video.currentTime = scrollPosition;
-}, 33.3)
-
-// controller about myself
-const container = document.querySelector(".main-container");
-const myself = document.querySelectorAll(".main-myself h1");
-const mainImg = document.querySelector(".main-img img");
-
-
-var timeLine = new TimelineMax();
-    timeLine.fromTo(myself, 1,{x:-500,y:0,opacity:0}, {x:0,duration:5,opacity:1,type:"soft",ease: "power1.out"})
-    timeLine.fromTo(mainImg, 1,{x:0,y:-450,opacity:0.8 }, {x:0,y:0,duration:5,type:"soft",opacity:1,ease:"back.out(1.5))"});
-
-const myselfController = new ScrollMagic.Controller();
-var sceneMyself = new ScrollMagic.Scene({
-    duration: 5000, // the scene should last for a scroll distance of
-    triggerElement: container,
-    triggerHook: 0,
-})
-    .setPin(container)
-    .setTween(timeLine)
-    .addTo(myselfController);
+// TweenMax.to(".overlay img",1, {
+//   repeat: 3,
+//   rotate: "360deg",
+//   ease: Expo.easeInOut,
+// });
 
 //menu-ham
-const menuHam = document.querySelector(".header-menu-ham");
+const menuHam = document.querySelector(".header__menu-ham");
+const menuBlock = document.querySelector(".header__menu-block");
 let menuHamOpen = false;
-menuHam.addEventListener('click', () => {
-    if (!menuHamOpen) {
-        menuHam.classList.add('active');
-        menuHamOpen = true;
-    } else {
-        menuHam.classList.remove('active');
-        menuHamOpen = false;
-    }
-})
+menuHam.addEventListener("click", () => {
+  if (!menuHamOpen) {
+    menuHam.classList.add("active");
+    menuBlock.classList.add("active");
+    menuHamOpen = true;
+  } else {
+    menuHam.classList.remove("active");
+    menuBlock.classList.remove("active");
+    menuHamOpen = false;
+  }
+});
