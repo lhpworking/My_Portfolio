caf5a504f87f9e9620909d37af8c57c1ffd42243
Phats Lys <lhpworking@gamil.com>
2021-11-22 18:45:25 +0700

    add some project

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,22 +6,28 @@ const myself = document.querySelectorAll(".main__myself h1");
 const myselfBtn = document.querySelector(".main__myself button");
 const mainImg = document.querySelector(".main__myself-img img");
 const projectImg = document.querySelector(".main__project-img img");
-
+const socialItem = document.querySelectorAll(".social__block-item");
 /** animation: section myself */
 gsap.fromTo(
   myself,
-  { x: -400, y: 0, duration: 4, opacity: 0, ease: Bounce },
-  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+  { x: -400, y: 0, duration: 4, opacity: 0, ease: "bounce" },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: "power1" }
 );
 gsap.fromTo(
   myselfBtn,
-  { x: 0, y: 200, duration: 4, opacity: 0, ease: Bounce },
-  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+  { x: 0, y: 200, duration: 4, opacity: 0, ease: "bounce" },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: "bounce" }
 );
 gsap.fromTo(
   mainImg,
-  { x: 0, y: -400, duration: 4, opacity: 0, ease: Bounce },
-  { x: 0, y: 0, duration: 4, opacity: 1, ease: Expo.easeInOut }
+  { x: 0, y: -400, duration: 4, opacity: 0, ease: "bounce" },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: "bounce" }
+);
+
+gsap.fromTo(
+  socialItem,
+  { x: 200, y: 0, duration: 4, opacity: 0, ease: "bounce" },
+  { x: 0, y: 0, duration: 4, opacity: 1, ease: "bounce" }
 );
 
 // overlay
@@ -39,18 +45,12 @@ TweenMax.to(".overlay", 4, {
   ease: Expo.easeInOut,
 });
 //menu-ham
-const menuHam = document.querySelector(".header__menu-ham");
+const menuInfo = document.querySelector("#menu-info");
 const menuBlock = document.querySelector(".header__menu-block");
-let menuHamOpen = false;
-menuHam.addEventListener("click", () => {
-  if (!menuHamOpen) {
-    menuBlock.classList.add("active");
-    document.querySelector("body").style.overflow = "hidden";
-    menuHamOpen = true;
-  } else {
-    menuBlock.classList.remove("active");
-    menuHamOpen = false;
-  }
+
+menuInfo.addEventListener("click", () => {
+  menuBlock.classList.toggle("active");
+  document.querySelector("body").style.overflow = "hidden";
 });
 // menu-close
 const menuClose = document.querySelector("#menu-close");
